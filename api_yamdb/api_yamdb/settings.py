5880e21f01dff40db3b57ff0282385e3835d6c73
Sergey Afonin <afoninsb@yandex.ru>
2022-12-17 21:00:44 +0300

    to first review

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -117,7 +117,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 UNUSED_USERNAME = 'me'
 
